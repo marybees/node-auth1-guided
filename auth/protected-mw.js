8fbab44ba48c46afe07a99c1238870fc1461aaa9
Mary Baker <meb601@gmail.com>
2020-10-13 09:59:16 -0400

    Add db3 file and user seed

--- auth/protected-mw.js
+++ auth/protected-mw.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
-  if (req.username) {
+  console.log("session", req.session);
+  if (req.session.username) {
     next();
   } else {
     res.status(401).json({ you: "cannot pass!" });
